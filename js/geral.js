90b4be353130d9e21b7744edb4cd58e2967b8aad
Eliel <elielcezar@gmail.com>
2018-05-15 15:25:31 -0300

    ajustes mobile e refatoração do CSS

--- js/geral.js
+++ js/geral.js
@@ -1,26 +1,12 @@
 
+(function($) {
+   $().ready(function() {
 
-jQuery(document).ready(function ($) {
+      	$('#nav-icon').click(function(){
+      		$('#nav-icon, .menu.nav').toggleClass('open');
+      	});
 
-	/* formulario de newsletter */
+				$('form input[type="submit"]').addClass('btn btn-primary btn-raised');
 
-	$('footer .newsletter #edit-mail').attr('placeholder','Insira seu email');
-
-
-  $('form input[type="submit"]').addClass('btn btn-primary btn-raised');  
-
-  //$('#block-views-exp-festivais-page-1 h2').prependTo('.views-exposed-form');
-
-  /*$('#edit-title').attr('placeholder','Nome do Festival');
-  $('#edit-field-cidade-tid').attr('placeholder','Pesquisa por Cidade');
-  $('#edit-field-estado-value option:first-child').html('Pesquisa por Região');
-  $('input[name="field_date_value[value][date]"]').attr('placeholder','Pesquisa por Data');*/
-
-  //$('#edit-field-date-value-value-datepicker-popup-0').css('width','100%');
-
-
-
-});
-
-
-
+    });
+})(jQuery);
--- templates/header.tpl.php
+++ templates/header.tpl.php
@@ -1,50 +1,37 @@
 
 <header>
-    <div class="container"> 
+    <div class="container">
 
+      <div id="nav-icon">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
 
-        <!-- Single button -->
-            <div class="btn-group" id="menu-toggle" onclick="openNav()">
-              <button type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="">
-                <div id="nav-icon2">
-                  <span></span>
-                    <span></span>
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                </div>
-              </button>
-            </div>
-
-          <div class="redes-sociais">
-                <a class="facebook" href="https://www.facebook.com/Psicodelia.org/" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
-                <a class="twitter" href="https://twitter.com/psicodelia" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                <a class="instagram" href="http://instagram.com/psicodelia_org" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                <a href="http://feeds.feedburner.com/psicodelia" class="rss" target="_blank"><i class="fa fa-rss" aria-hidden="true"></i></a>
-                <!--a href="#" data-toggle="modal" data-target="#cadastro-newsletter" class="rss"><i class="fa fa-envelope-o" aria-hidden="true"></i></a-->
-            </div>
-        	
-    		<a href="<?php print base_path()?>"><img src="<?php print base_path() . path_to_theme() ?>/img/psicodelia.png" class="logo" /></a>
-
-        <!-- The overlay -->
-        <div id="myNav" class="overlay">
-
-          <!-- Overlay content -->
-          <div class="overlay-content">
-            <a href="<?php print $front_page; ?>">Home</a>
-            <a href="<?php print $front_page; ?>noticias">Notícias</a>
-            <a class="destaque" href="http://loja.psicodelia.org">Loja</a>
-            <a href="#" data-toggle="modal" data-target="#enviar-noticia">Envie uma Notícia</a>
-            <a href="<?php print $front_page; ?>festivais" target="_blank">Festivais de Psytrance</a>
-            <!--a href="<?php print $front_page; ?>anuncie-no-psicodelia">Anuncie</a-->
-          </div>
-
+        <div class="redes-sociais">
+              <a class="facebook" href="https://www.facebook.com/Psicodelia.org/" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
+              <a class="twitter" href="https://twitter.com/psicodelia" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+              <a class="instagram" href="http://instagram.com/psicodelia_org" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+              <a href="http://feeds.feedburner.com/psicodelia" class="rss" target="_blank"><i class="fa fa-rss" aria-hidden="true"></i></a>
+              <!--a href="#" data-toggle="modal" data-target="#cadastro-newsletter" class="rss"><i class="fa fa-envelope-o" aria-hidden="true"></i></a-->
         </div>
 
-        <?php print render($page['topo']);  ?> 
+    		<a href="<?php print base_path()?>"><img src="<?php print base_path() . path_to_theme() ?>/img/psicodelia.png" class="logo" /></a>
+
+          <!-- Overlay content -->
+          <ul class="menu nav">
+            <li><a href="<?php print $front_page; ?>">Home</a></li>
+            <li><a href="<?php print $front_page; ?>noticias">Notícias</a></li>
+            <li><a class="destaque" href="http://loja.psicodelia.org">Loja</a></li>
+            <li><a href="#" data-toggle="modal" data-target="#enviar-noticia">Envie uma Notícia</a></li>
+            <li><a href="<?php print $front_page; ?>festivais" target="_blank">Festivais de Psytrance</a></li>
+            <!--li><a href="<?php print $front_page; ?>anuncie-no-psicodelia">Anuncie</a></li-->
+          </ul>
+
+        <?php print render($page['topo']);  ?>
 
 
-      
+
     </div>
 </header>
